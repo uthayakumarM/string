2de399e0f2689eea8ecabe37e54ba8b458949fc4
Elon Musk <ceo@twitter.com>
2022-12-15 13:20:52 +0000

    Implemented my_strlen function

---
+++ string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int my_strlen(char *s)
+{
+	char *p = s;
+
+	while (*p)
+		++p;
+
+	return (p -s);
+
+}
+int main(void)
+{
+	int i;
+	char *s[] = {
+		"Git tutorials",
+		"Tutorials Point"
+	};
+
+	for (i = 0; i < 2; ++1)
+		printf(("String length of %s = %d\n", s[i], my_strlen(s[i]));
+
+				return 0;
+				
+				}
+
